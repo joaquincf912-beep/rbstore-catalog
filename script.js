a53601b7d9d3677d505628919a3ec41ed9a996b9
joaquincf912-beep <joaquincf912-beep@users.noreply.github.com>
2026-09-15 19:59:13 -0400

    feat: actualizar imágenes reales para Cargador 20w, Panel LED RGB, AirPods Pro 3gen, AirPods Pro Max y Cable Lightning

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 const RBSTORE_CONFIG = {
   whatsappNumber: "584120500675",
   adminKey: "2828",
-  storageKey: "rbstore_catalog_v11"
+  storageKey: "rbstore_catalog_v12"
 };
 
 // INITIAL SHOWCASE CATALOG (In exact order specified by user)
