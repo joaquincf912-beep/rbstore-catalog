3eb95dce23928c521705c0cb6e3366df48dcb6e0
joaquincf912-beep <joaquincf912-beep@users.noreply.github.com>
2026-09-15 21:04:53 -0400

    feat: replace Cargador Dynamic 60W image with new user provided photo

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 const RBSTORE_CONFIG = {
   whatsappNumber: "584120500675",
   adminKey: "2828",
-  storageKey: "rbstore_catalog_v28"
+  storageKey: "rbstore_catalog_v29"
 };
 
 // INITIAL SHOWCASE CATALOG (In exact order specified by user)
