0afa8f6b838eabde94201f33e649bf3ff33e130c
joaquincf912-beep <joaquincf912-beep@users.noreply.github.com>
2026-09-15 20:52:13 -0400

    feat: update product images for AirPods Pro Max and Cable Lightning Type C

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 const RBSTORE_CONFIG = {
   whatsappNumber: "584120500675",
   adminKey: "2828",
-  storageKey: "rbstore_catalog_v25"
+  storageKey: "rbstore_catalog_v27"
 };
 
 // INITIAL SHOWCASE CATALOG (In exact order specified by user)
