3092f628ca2c0c748a7343b8956eee986e80f3ee
joaquincf912-beep <joaquincf912-beep@users.noreply.github.com>
2026-09-15 20:14:27 -0400

    fix: reemplazar emojis por íconos SVG vectoriales profesionales en las tarjetas de contacto y características

--- app.js
+++ app.js
@@ -5,7 +5,7 @@
 const RBSTORE_CONFIG = {
   whatsappNumber: "584120500675",
   adminKey: "2828",
-  storageKey: "rbstore_catalog_v16"
+  storageKey: "rbstore_catalog_v17"
 };
 
 // INITIAL SHOWCASE CATALOG (In exact order specified by user)
