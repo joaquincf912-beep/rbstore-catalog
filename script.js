154e4b1a2771b28a77c0d45b409b66924eec115f
joaquincf912-beep <joaquincf912-beep@users.noreply.github.com>
2026-09-15 20:09:19 -0400

    fix: cambiar título principal de 'tecnología' a 'accesorios'

--- script.js
+++ script.js
@@ -5,7 +5,7 @@
 const RBSTORE_CONFIG = {
   whatsappNumber: "584120500675",
   adminKey: "2828",
-  storageKey: "rbstore_catalog_v14"
+  storageKey: "rbstore_catalog_v15"
 };
 
 // INITIAL SHOWCASE CATALOG (In exact order specified by user)
